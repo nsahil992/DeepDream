2894468b389e5b8a842f34dd91a0d615ffac9c17
Sahil Naik <nsahil992@gmail.com>
2023-10-31 18:28:10 +0530

    Modified

--- code.jl
+++ code.jl
@@ -3,3 +3,8 @@ using Flux: @epochs
 using Statistics, PyCall, Flux.Tracker
 using Images: RGB
 
+np = pyimport("numpy")
+Image = pyimport("PIL.image.jpeg")
+
+function deprocess_and_pillow(img)
+    μ, σ = ([0.485])
