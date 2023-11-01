4a4cb63ee0c21742e074ef6f5c4b31dd3d476ec8
Sahil Naik <nsahil992@gmail.com>
2023-11-01 21:00:40 +0530

    Modified

--- code.jl
+++ code.jl
@@ -8,4 +8,7 @@ Image = pyimport("PIL.image.jpeg")
 
 function deprocess_and_pillow(img)
     μ, σ = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    rbg = cat(collect(map(x -> (img[:, :, x, 1] .* σ[x]) .+  μ[x])))
+    rgb = cat(collect(map(x -> (img[:, :, x, 1] .* σ[x]) .+  μ[x], 1:3))..., dims=3)
+    rgb = np.uint8(np.interp(np.clip(rgb ./ 255, -1, 1), (-1, 1), (0, 255)))
+    return
+    Image.formarray(rgb)
