1f68ccb654f13bb3d037b6c05d1551a6786a15dd
Sahil Naik <nsahil992@gmail.com>
2023-11-01 20:58:19 +0530

    Modified

--- code.jl
+++ code.jl
@@ -7,4 +7,5 @@ np = pyimport("numpy")
 Image = pyimport("PIL.image.jpeg")
 
 function deprocess_and_pillow(img)
-    μ, σ = ([0.485])
+    μ, σ = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    rbg = cat(collect(map(x -> (img[:, :, x, 1] .* σ[x]) .+  μ[x])))
