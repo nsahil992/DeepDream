99af12517be62cfb9ee9ae8603697cbf6b02b18c
Sahil Naik <nsahil992@gmail.com>
2023-11-05 21:24:25 +0530

    Modified

--- code.jl
+++ code.jl
@@ -1,10 +1,13 @@
 using Metalhead: VGG19, preprocess, load
 using Flux: @epochs
+using Foo: bar, baz
 using Statistics, PyCall, Flux.Tracker
 using Images: RGB
+#packages imported
 
 np = pyimport("numpy")
 Image = pyimport("PIL.image.jpeg")
+
 
 function deprocess_and_pillow(img)
     μ, σ = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
